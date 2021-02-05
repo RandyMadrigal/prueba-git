38f4b753cd0fa65377c8c4a8b0f28b6d0be85559
Chan <randymadrigal.com>
2021-02-05 18:58:09 -0400

    "Modificacion numero 5"

--- src/hola_mundo/Saludo.java
+++ src/hola_mundo/Saludo.java
@@ -1,13 +1,32 @@
 package hola_mundo;
 
+import java.util.Scanner;
+
 public class Saludo {
 	
 	public static void main(String []args) {
+		Scanner dato = new Scanner(System.in);
+		
 		
 		System.out.println("Hello world");
 		System.out.println("Ver modificacion en bash de git");
 		System.out.println("Cambio de usuario en git");
+
+		int x,y,resultado;
 		
+		System.out.println("Ingrese el valor de x: ");
+		x = dato.nextInt();
+		
+		System.out.println("Ingrese el valor de x: ");
+		y = dato.nextInt();
+		
+		
+		resultado = x+y;
+	
+		System.out.println("El resultado es: "+resultado);
+		
+		
+		dato.close();
 	}
 
 }
